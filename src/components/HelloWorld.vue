9c23a83eaa3c280b20d5b3ba8975a2f34de23939
haidi <haidinurhadinata@gmail.com>
2022-02-05 13:56:21 +0800

    pertamax

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h2>Setup Project Web MIS</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
